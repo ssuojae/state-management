c83b8ffce4e26b4eee081901a0182fb81f41a0c4
suojae3 <suojae3@gmail.com>
2024-07-22 21:35:12 +0900

    :sparkles: feat: LocalDataSource 구현체 추가

---
+++ bloc/lib/data/local/local_data_source_imp.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+import 'shared_preference_service.dart';
+
+import '../../../domain/todo_entity.dart';
+import '../../../util/todo_error.dart';
+import '../../../util/todo_exception.dart';
+import '../local/local_data_source_interface.dart';
+
+final class LocalDataSource implements ILocalDataSource {
+  final SharedPreferencesService prefsService;
+
+  LocalDataSource(this.prefsService);
+
+  @override
+  Future<List<TodoEntity>> loadTodos() async {
+    try {
+      String? jsonString = prefsService.getString('todos');
+      if (_isValidJsonString(jsonString)) {
+        return _convertJsonListToTodos(json.decode(jsonString!));
+      }
+    } catch (error) {
+      throw TodoException(TodoError.loadError("Error loading todos: $error"));
+    }
+    return [];
+  }
+
+  @override
+  Future<void> saveTodos(List<TodoEntity> todos) async {
+    try {
+      List<Map<String, dynamic>> jsonList = todos.map((todo) => todo.toJson()).toList();
+      await prefsService.setString('todos', json.encode(jsonList));
+    } catch (error) {
+      throw TodoException(TodoError.saveError("Error saving todos: $error"));
+    }
+  }
+
+  @override
+  Future<void> saveTodo(TodoEntity todo) async {
+    try {
+      List<TodoEntity> currentTodos = await loadTodos();
+      currentTodos.add(todo);
+      await saveTodos(currentTodos);
+    } catch (error) {
+      throw TodoException(TodoError.saveError("Error saving todo: $error"));
+    }
+  }
+
+  @override
+  Future<void> updateTodo(TodoEntity updatedTodo) async {
+    try {
+      List<TodoEntity> currentTodos = await loadTodos();
+      final index = currentTodos.indexWhere((todo) => todo.id == updatedTodo.id);
+      if (index != -1) {
+        currentTodos[index] = updatedTodo;
+        await saveTodos(currentTodos);
+      }
+    } catch (error) {
+      throw TodoException(TodoError.saveError("Error updating todo: $error"));
+    }
+  }
+
+  @override
+  Future<void> deleteTodoById(String id) async {
+    try {
+      List<TodoEntity> currentTodos = await loadTodos();
+      currentTodos.removeWhere((todo) => todo.id == id);
+      await saveTodos(currentTodos);
+    } catch (error) {
+      throw TodoException(TodoError.saveError("Error deleting todo: $error"));
+    }
+  }
+
+  @override
+  Future<void> clearTodos() async {
+    try {
+      await prefsService.remove('todos');
+    } catch (error) {
+      throw TodoException(TodoError.clearError("Error clearing todos: $error"));
+    }
+  }
+
+  bool _isValidJsonString(String? jsonString) {
+    return jsonString != null && jsonString.isNotEmpty;
+  }
+
+  List<TodoEntity> _convertJsonListToTodos(List<dynamic> jsonList) {
+    return jsonList.map((item) => TodoEntity.fromJson(item as Map<String, dynamic>)).toList();
+  }
+}
