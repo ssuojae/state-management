7941ee37c674b8c67c92bb68685bca8bfc303dc7
suojae3 <suojae3@gmail.com>
2024-07-22 22:40:55 +0900

    :sparkles: feat: TodoState 추가

---
+++ cubit/lib/presentation/viewmodel/state/todo_state.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:todo_state_management/domain/todo_entity.dart';
+
+part 'todo_state.freezed.dart';
+
+@freezed
+abstract class TodoState with _$TodoState {
+  const factory TodoState.loading() = TodoLoading;
+  const factory TodoState.loaded(List<TodoEntity> todos) = TodoLoaded;
+  const factory TodoState.error(String message) = TodoErrorState;
+}
